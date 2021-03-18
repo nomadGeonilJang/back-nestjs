2dcd749f6594c3b058f887ef2777687233d9f113
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-18 21:28:13 +0900

    add more routers

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('movies') //base path
 export class MoviesController {
@@ -11,5 +19,21 @@ export class MoviesController {
   getOne(@Param('id') movieId: string) {
     console.log(movieId);
     return `this is return one movie--> ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return 'this is post request';
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') movieId: string) {
+    return `this is delete ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `this is patch ${movieId}`;
   }
 }
